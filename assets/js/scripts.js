4e9c7caed5e50eccc6ea79ee1e916e6779be4127
Byron Wall <byrondevonwall@gmail.com>
2016-02-17 17:03:01 -0500

    Add functional meow keyboard

---
+++ assets/js/scripts.js
@@ -0,0 +1,94 @@
+$(function(){
+  //first meow
+var meow01 = $(".meow01");
+var playMeow01 = meow01.find("audio")[0];
+meow01.hover(function(){
+  playMeow01.play();
+});
+
+var meow02 = $(".meow02");
+var playMeow02 = meow02.find("audio")[0];
+meow02.hover(function(){
+  playMeow02.play();
+});
+
+var meow03 = $(".meow03");
+var playMeow03 = meow03.find("audio")[0];
+meow03.hover(function(){
+  playMeow03.play();
+});
+
+var meow04 = $(".meow04");
+var playMeow04 = meow04.find("audio")[0];
+meow04.hover(function(){
+  playMeow04.play();
+});
+
+var meow05 = $(".meow05");
+var playMeow05 = meow05.find("audio")[0];
+meow05.hover(function(){
+  playMeow05.play();
+});
+
+var meow06 = $(".meow06");
+var playMeow06 = meow06.find("audio")[0];
+meow06.hover(function(){
+  playMeow06.play();
+});
+
+var meow07 = $(".meow07");
+var playMeow07 = meow07.find("audio")[0];
+meow07.hover(function(){
+  playMeow07.play();
+});
+
+var meow08 = $(".meow08");
+var playMeow08 = meow08.find("audio")[0];
+meow08.hover(function(){
+  playMeow08.play();
+});
+
+var meow09 = $(".meow09");
+var playMeow09 = meow09.find("audio")[0];
+meow09.hover(function(){
+  playMeow09.play();
+});
+
+var meow10 = $(".meow10");
+var playMeow10 = meow10.find("audio")[0];
+meow10.hover(function(){
+  playMeow10.play();
+});
+
+var meow11 = $(".meow11");
+var playMeow11 = meow11.find("audio")[0];
+meow11.hover(function(){
+  playMeow11.play();
+});
+
+var purr1 = $(".purr1");
+var playPurr1 = purr1.find("audio")[0];
+purr1.hover(function(){
+  playPurr1.play();
+});
+
+var purr2 = $(".purr2");
+var playPurr2 = purr2.find("audio")[0];
+purr2.hover(function(){
+  playPurr2.play();
+});
+
+var purr3 = $(".purr3");
+var playPurr3 = purr3.find("audio")[0];
+purr3.hover(function(){
+  playPurr3.play();
+});
+
+var hiss1 = $(".hiss1");
+var playHiss1 = hiss1.find("audio")[0];
+hiss1.hover(function(){
+  playHiss1.play();
+});
+
+
+});
